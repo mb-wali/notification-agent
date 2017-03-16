ad4d7bc68a950bf84476e2a54a9c0e63c3dc6416
Ian McEwen <mian@cyverse.org>
2017-03-16 12:58:23 -0700

    Upgrade common-cli

--- project.clj
+++ project.clj
@@ -23,7 +23,7 @@
                                [com.fasterxml.jackson.core/jackson-core]]]
                  [compojure "1.5.0"]
                  [org.cyverse/clojure-commons "2.8.0"]
-                 [org.cyverse/common-cli "2.8.0"]
+                 [org.cyverse/common-cli "2.8.1"]
                  [org.cyverse/kameleon "3.0.0"]
                  [org.cyverse/service-logging "2.8.0"]
                  [org.cyverse/event-messages "0.0.1"]
